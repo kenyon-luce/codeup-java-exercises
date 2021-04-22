84fe82675e4a9102dbed41d92cc9ed989a7a8a05
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-22 16:13:24 -0500

    finished experimentation exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
+//        ^prints a new empty line after printing this
+
+        System.out.print("Hello, World!");
+//        ^prints this without creating a new line
+        System.out.print("\n");
+//        ^by adding another one that adds "\n", we are forcing the line to break and are now getting the same result as .println
     }
 }
+
+//in order for a java file to run, it must be in a main method (sorta like a function)
+//the main method has to have the same name as the file it is in, else you will get an error
+//when a java file runs in a repo for the first time, an "out" folder is created, which stores the compiled version of the file separate from "src"
+//each line of code NEEDS to have a ";" at the end of it, else you will get an error
