99ffb57a42e13b79a1ce616fe1aa896e36cec616
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-23 11:52:56 -0500

    finished exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,59 @@ public class HelloWorld {
 //        ^prints this without creating a new line
         System.out.print("\n");
 //        ^by adding another one that adds "\n", we are forcing the line to break and are now getting the same result as .println
+
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "this is my string";
+        //because we told it to be a string, we can only assign a string to it
+        System.out.println(myString);
+
+        float myNumber = 3;
+        System.out.println(myNumber);
+        //can't log an unassigned variable
+        //depending on the data type, you can only assign an INTEGER within its specific range
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+        //increment applies before the x, that's why you get 6 in both instances compared to the code above
+//        System.out.println(x);
+
+        //can't make a variable named class cuz we're in one lol
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        //error: exception in thread
+
+//        int three = (int) "three";
+        //error: string cannot be converted to int
+
+//        int x = 4;
+////        x = x + 5;
+//        x += 5;
+        //shorthand operators work here
+
+//        int x = 900000000;
+//        int y = 4;
+////        y = y * x;
+//        y *= x; //-694967296
+//        System.out.println(y);
+        //if we increment a numerical variable to a number larger that the int can hold, it will return a strange negative number
+
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+////        y = y - x;
+//        x /= y; // 10 / 2 = 5
+//        y -= x; // 2 - 5 = -3
+//        System.out.println(x);
+//        System.out.println(y);
+
     }
 }
 
