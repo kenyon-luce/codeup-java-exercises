3f180e5e396597941baaaf8835eb6aca31ae2d70
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-23 09:19:07 -0500

    updated notes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,6 +11,7 @@ public class HelloWorld {
 }
 
 //in order for a java file to run, it must be in a main method (sorta like a function)
-//the main method has to have the same name as the file it is in, else you will get an error
+//the class has to have the same name as the file it is in, else you will get an error
+//to make the code run in the class, we need to use "main" method
 //when a java file runs in a repo for the first time, an "out" folder is created, which stores the compiled version of the file separate from "src"
 //each line of code NEEDS to have a ";" at the end of it, else you will get an error
