23829fb0621d28e05228eccbcc325978ba1c74c5
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-26 16:17:12 -0500

    finished exercise 4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args){
-//        ****************EXERCISE 3: Table of Powers*****************
+//        ****************EXERCISE*****************
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hi! I'm a loop, how many numbers should I loop?");
-        int input = sc.nextInt();
-        System.out.println("number | squared | cubed");
-        System.out.println("------ | ------- | -----");
-        for(int i = 1; i <= input; i++){
-            int number = i;
-            int squared = i * i;
-            int cubed = i * i * i;
-            System.out.printf("%d      | %d       | %d\n", number, squared, cubed);
+        System.out.println("Please input your grade");
+        int grade = sc.nextInt();
+        if(grade >= 88){
+            System.out.println("A");
+        } else if(grade >= 80){
+            System.out.println("B");
+        } else if(grade >= 67){
+            System.out.println("C");
+        }else if(grade >= 60){
+            System.out.println("D");
+        }else{
+            System.out.println("F");
         }
-        System.out.print("Here you go!");
     }
 }
