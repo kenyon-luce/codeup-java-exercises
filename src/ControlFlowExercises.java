c891437cf725845c0fb172518b17f94226dca55a
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-26 16:15:05 -0500

    finished exercise 3 (want to fix format)

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,19 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args){
-//        ****************EXERCISE 2: FizzBuzz*****************
-        for(int i = 1; i <= 100; i++){
-            if(i % 3 == 0 && i % 5 == 0){
-                System.out.println("FizzBuzz");
-            } else if(i % 3 == 0){
-                System.out.println("Fizz");
-            } else if(i % 5 == 0){
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+//        ****************EXERCISE 3: Table of Powers*****************
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hi! I'm a loop, how many numbers should I loop?");
+        int input = sc.nextInt();
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for(int i = 1; i <= input; i++){
+            int number = i;
+            int squared = i * i;
+            int cubed = i * i * i;
+            System.out.printf("%d      | %d       | %d\n", number, squared, cubed);
         }
+        System.out.print("Here you go!");
     }
 }
