bbfccde83efbd91a5a99a25620aa972a4d8398cc
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-28 10:31:43 -0500

    updated exercise 5: attempted bonus

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -66,43 +66,63 @@ public class MethodsExercises {
     }
 
     //EXERCISE 4
-    public static void rollDice(){
+    public static void rollDice() {
         String response;
-        do{
+        do {
             Scanner sc = new Scanner(System.in);
             System.out.println("You are rolling a pair of dice, how many sides do both dies have?");
             int sides = sc.nextInt();
-            int die1 = (int)(Math.random() * (sides - 1) + 1);
+            int die1 = (int) (Math.random() * (sides - 1) + 1);
             //because we are getting a number, we need to make sure it reads as an integer by adding (int) before the actual variable value
-            int die2 = (int)(Math.random() * (sides - 1) + 1);
+            int die2 = (int) (Math.random() * (sides - 1) + 1);
             System.out.println(die1 + "  " + die2);
             int result = die1 + die2;
             System.out.println("You rolled a " + result + "!");
 
             System.out.println("Re-roll?  [Y/N]");
             response = sc.next();
-        } while(response.equalsIgnoreCase("Y"));
-        }
+        } while (response.equalsIgnoreCase("Y"));
+    }
+
     //EXERCISE 5
-    public static void HighLow(){
+    public static void HighLow() {
         Scanner sc = new Scanner(System.in);
-        int randomNum = (int)(Math.random() * (100 - 1) + 1);
+        int randomNum = (int) (Math.random() * (100 - 1) + 1);
         System.out.println("Guess the number!");
 //        System.out.println("(" + randomNum + ")");
         //^this was just to test the game
 
-        int guess;
-        //^the variable name is outside so we can call on it later
-        do{
-            guess = sc.nextInt();
-            //^giving it a value here lets you update to a new guess since it loops
-            if(guess > randomNum){
-                System.out.println("HIGH");
-            } else if(guess < randomNum) {
-                System.out.println("LOW");
-            }
-        } while(guess != randomNum);
-        //^as you guess wrong, it will return the relevant response and will loop again. When you guess the correct number, you will exit the loop and get the following response
+        int guessLimit = 5;
+        int guesses = 0;
+        int guess; //the variable name is outside so we can call on it later
+            do {
+                guess = sc.nextInt(); ///giving it a value here lets you update to a new guess since it loops
+                if (guess > randomNum) {
+                    System.out.println("HIGH");
+                } else if (guess < randomNum) {
+                    System.out.println("LOW");
+                }
+                guesses++;
+                System.out.println("Guesses left: " + (guessLimit - guesses));
+            } while (guess != randomNum);
+            //^as you guess wrong, it will return the relevant response and will loop again. When you guess the correct number, you will exit the loop and get the following response
             System.out.println("GOOD GUESS!");
+
+//        int guessesLeft;
+//        if (guessesLeft == 0){
+//            do {
+//                guess = sc.nextInt();
+//                if (guess > randomNum) {
+//                    System.out.println("HIGH");
+//                } else if (guess < randomNum) {
+//                    System.out.println("LOW");
+//                }
+//                guesses++;
+//                System.out.println("Guesses left: " + (guessLimit - guesses));
+//            } while (guess != randomNum);
+//            System.out.println("GOOD GUESS!");
+//        }
+
     }
+
 }
