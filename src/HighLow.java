bfc55e90307838503bfcfa876fa79f2a2178034f
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-29 09:46:43 -0500

    paused on bonus

--- src/HighLow.java
+++ src/HighLow.java
@@ -13,8 +13,8 @@ public class HighLow {
 //        System.out.println("(" + randomNum + ")");
         //^this was just to test the game
 
-        int guessLimit = 5;
-        int guesses = 0;
+//        int guessLimit = 5;
+//        int guesses = 0;
         int guess; //the variable name is outside so we can call on it later
         do {
             guess = sc.nextInt(); ///giving it a value here lets you update to a new guess since it loops
@@ -23,8 +23,8 @@ public class HighLow {
             } else if (guess < randomNum) {
                 System.out.println("LOW");
             }
-            guesses++;
-            System.out.println("Guesses left: " + (guessLimit - guesses));
+//            guesses++;
+//            System.out.println("Guesses left: " + (guessLimit - guesses));
         } while (guess != randomNum);
         //^as you guess wrong, it will return the relevant response and will loop again. When you guess the correct number, you will exit the loop and get the following response
         System.out.println("GOOD GUESS!");
