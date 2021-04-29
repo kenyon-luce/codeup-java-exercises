709a897d82d1711d0e3a4bd2fb625436686855ed
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-29 10:20:52 -0500

    finished exercise 1

---
+++ src/StringExercise.java
@@ -0,0 +1,11 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String string = "We don't need no education\nWe don't need no thought control";
+        string = "Check \"this\" out!, \"s inside of \"s!";
+        string = "In windows, the main drive is usually C:\\";
+        string = "In windows, the main drive is usually C:\\";
+        string = "I can do backslashes \\, double backslashes \\\\,\n and the amazing triple backslash \\\\\\!";
+        //so basically, if you ever want to add a bunch of backslashes for some reason, just take the amount of slashes you want to put and double that
+        System.out.println(string);
+    }
+}
