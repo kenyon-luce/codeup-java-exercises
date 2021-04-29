05657413747ad4bd98fd1fcbdffd48007c0d13c0
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-29 13:49:09 -0500

    fixed format real quick

--- src/Bob.java
+++ src/Bob.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 
 public class Bob {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("This is Bob, he is a lackadaisical teenager\nhence, he is very limited in conversation...");
-        while(true){
+        while (true) {
             String interact = sc.nextLine();
-            if(interact.endsWith("?")){
+            if (interact.endsWith("?")) {
                 System.out.println("Sure.");
-            } else if(interact.endsWith("!")){
+            } else if (interact.endsWith("!")) {
                 System.out.println("Woah, chill out!");
-            } else if(interact.equals("")){
+            } else if (interact.equals("")) {
                 System.out.println("*stares aggressively*");
                 System.out.println("Fine. Be that way!");
             } else {
                 System.out.println("Whatever.");
             }
         }
-        }
-
+    }
 }
