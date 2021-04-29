fbaa8efef62f109d20133c09c147af2fc495cac3
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-29 13:48:31 -0500

    feat: turned into loop for continuous conversation

--- src/Bob.java
+++ src/Bob.java
@@ -4,16 +4,19 @@ public class Bob {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("This is Bob, he is a lackadaisical teenager\nhence, he is very limited in conversation...");
-        String interact = sc.nextLine();
-        if(interact.endsWith("?")){
-            System.out.println("Sure.");
-        } else if(interact.endsWith("!")){
-            System.out.println("Woah, chill out!");
-        } else if(interact.equals("")){
-            System.out.println("*stares*");
-            System.out.println("Fine. Be that way!");
-        } else {
-            System.out.println("Whatever.");
+        while(true){
+            String interact = sc.nextLine();
+            if(interact.endsWith("?")){
+                System.out.println("Sure.");
+            } else if(interact.endsWith("!")){
+                System.out.println("Woah, chill out!");
+            } else if(interact.equals("")){
+                System.out.println("*stares aggressively*");
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
         }
-    }
+        }
+
 }
