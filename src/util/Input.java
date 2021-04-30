0766b903e58a931ae84cb2437612cb5829e12655
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-30 14:33:05 -0500

    updated exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,9 +1,45 @@
 package util;
 
+import java.util.Scanner;
+
+//class
 public class Input {
-    private String scanner;
+    //instance variable
+    private Scanner sc;
 
-    public static void main(String[] args){
+    //instance methods
+    public static String getString(){
+        return sc.nextLine();
+    };
 
+    //TODO: The yesNo method should return true if the user enters y, yes, or variants thereof, and false otherwise.
+    public static boolean yesNo(){
+        if(sc.next().startsWith("y")){
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    //TODO: The getInt(int min, int max) method should keep prompting the user for input until they give an integer within the min and max.
+    public static int getInt(int min, int max){
+        min = 1;
+        max = 10;
+    };
+
+    public static int getInt(){
+        return;
+    };
+
+    //TODO: The getDouble method should do the same thing, but with decimal numbers.
+    public static double getDouble(double min, double max){
+        min = 3.14;
+        max = 31.4;
+    };
+
+    public static double getDouble(){
+        return;
     }
+
+    //TODO: Create another class named InputTest that has a static main method that uses all the methods from the Input class.
 }
