925c740a6c53ea280ce1ea9f4bcba67026703156
Kenyon Luce <kenyon.luce02@gmail.com>
2021-04-30 12:45:29 -0500

    updated person exercise, started input exercise

--- src/Person.java
+++ src/Person.java
@@ -4,7 +4,7 @@ public class Person {
 
     private String name;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Person kenyon = new Person("Kenyon Luce");
         //^refers to the below constructor
         System.out.println(kenyon.getName());
@@ -12,23 +12,33 @@ public class Person {
         kenyon.sayHello();
         kenyon.setName("not kenyon");
         kenyon.sayHello();
+        //result will be different since we updated the name
         //we can do this with more people as well
     }
 
+
+    //    The class should have a constructor that accepts a `String` value and sets
+//    the person's name to the passed string.
+//
+//    Create a `main` method on the class that creates a new `Person` object and
+//    tests the above methods.
     //constructor allows us to to create a new person
+
     public Person(String name) {
         this.name = name;
     }
 
-    public String getName(){
+    public String getName() {
 //TODO: return the person's name
         return this.name;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
 //TODO: change the name property to the passed value
         this.name = name;
     }
-    public void sayHello(){
+
+    public void sayHello() {
 //TODO: print a message to the console using the person's name
         System.out.println("Hello " + getName() + "!");
     }
---
+++ src/util/Input.java
@@ -0,0 +1,9 @@
+package util;
+
+public class Input {
+    private String scanner;
+
+    public static void main(String[] args){
+
+    }
+}
