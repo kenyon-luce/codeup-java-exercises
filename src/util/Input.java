ea7788ca10eab2a3462dbb72f8d9f8f622876e04
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-03 09:57:23 -0500

    got most methods working (decimal still needs work)

--- src/util/Input.java
+++ src/util/Input.java
@@ -10,6 +10,7 @@ public class Input {
     //instance methods
     public static String getString(){
         //because we are creating an instance to an already defined instance type (scanner), we do not need the "static" keyword
+        System.out.println("Give me a string");
         String string = sc.nextLine();
         System.out.println(string);
         return string;
@@ -18,6 +19,7 @@ public class Input {
     //TODO: The yesNo method should return true if the user enters y, yes, or variants thereof, and false otherwise.
     public static boolean yesNo(){
         //because we are creating an instance to an already defined instance type (scanner), we do not need the "static" keyword
+        System.out.println("Continue?");
         String input = sc.next();
         if(input.startsWith("y")){
             System.out.println("true");
@@ -50,8 +52,8 @@ public class Input {
     //TODO: The getDouble method should do the same thing, but with decimal numbers.
 
     public static double getDouble(double min, double max) {
-//        min = 1;
-//        max = 10;
+        //this method breaks if i input a decimal number
+        System.out.println("Give me a decimal number between " + max + " and " + min);
         double guessDouble;
         do {
             guessDouble = getDouble();
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,15 +1,10 @@
 package util;
 
 public class InputTest {
-    public static void main(String[] args){
-//        while (Input.yesNo() == true) {
-        System.out.println("Type a number here");
-//        Input.getString();
-        Input.getInt(1,10);
-//        Input.getDouble(1, 10);
-
-//            System.out.println("Continue?");
-//            Input.yesNo();
-        }
-//    }
+    public static void main(String[] args) {
+        Input.getString();
+        Input.yesNo();
+        Input.getInt(1, 10);
+        Input.getDouble(1, 10);
+    }
 }
