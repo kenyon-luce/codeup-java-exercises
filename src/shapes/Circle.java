a929aad87ec9bbe6b274f331d7b0e60d92f4e402
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-03 10:27:51 -0500

    circle exercise wip

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,19 @@
 package shapes;
 
+import java.util.Scanner;
+
 public class Circle {
+    private double radius;
+
+    public Circle(double radius){
+        System.out.println("This is a circle with a radius of: " + radius);
+    }
+    public double getArea(){
+        double area = Math.pow(radius, 2) * Math.PI;
+        return area;
+    }
+    public double getCircumference(){
+        double circum = (radius * 2) * Math.PI;
+        return circum;
+    }
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,5 @@
 package shapes;
 
 public class CircleApp {
+//    System.out.println(Circle(5));
 }
