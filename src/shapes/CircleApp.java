b452310ba9cda88f0119104af37ab7c6e8c3addc
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-03 12:27:08 -0500

    refactored count

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -5,20 +5,20 @@ import util.Input;
 import java.util.Scanner;
 
 public class CircleApp {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int count = 0;
+    private static int count = 0;
 
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
         do {
             System.out.println("What is the radius of your circle?");
             double input = Input.getDouble();
             System.out.printf("Radius: %.2f\n", input);
-
             Circle newCircle = new Circle(input);
             newCircle.getArea();
             newCircle.getCircumference();
             count++;
         } while (Input.yesNo());
+
         System.out.println("You finished making " + count + " circles");
     }
 }
