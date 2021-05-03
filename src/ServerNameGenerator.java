6b34fe7d9d3e20f30d8577c907df5ae7ceaebf40
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-03 17:01:30 -0500

    updated name generator exercise

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,8 +1,17 @@
+import java.util.Arrays;
 import java.util.Random;
 
+
 public class ServerNameGenerator {
-    String[] adjectives = {"sad", "happy", "wise", "foolish", "fast", "slow", "red", "orange", "yellow", "green", "blue", "purple",};
-    String[] nouns = {"wolf", "moose", "rooster", "Dan", "Bert", "Joe", "car", "bus", "plane", "bike", "house", "lily"};
-    Random randomAdj = new Random(adjectives.length);
-    Random randomNoun = new Random(nouns.length);
+    public static void main(String[] args){
+        String[] adjectives = {"sad", "happy", "wise", "foolish", "fast", "slow", "red", "orange", "yellow", "green", "blue", "purple",};
+        String[] nouns = {"wolf", "moose", "rooster", "Dan", "Bert", "Joe", "car", "bus", "plane", "bike", "house", "lily"};
+        Random randomAdj = new Random(adjectives.length);
+        Random randomNoun = new Random(nouns.length);
+
+        System.out.println(randomAdj);
+        System.out.println(randomNoun);
+    }
 }
+
+
