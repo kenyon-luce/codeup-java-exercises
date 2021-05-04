3a6caa6873fb188d081b7a1b7000b15c969aaa3e
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-04 14:18:49 -0500

    added jet

--- src/InheritanceAndPolymorphism/InAndPolyTester.java
+++ src/InheritanceAndPolymorphism/InAndPolyTester.java
@@ -12,5 +12,10 @@ public class InAndPolyTester {
         bus.honkHorn();
         bus.setNumberOfOccupants(2);
         System.out.println("Zooming in a bus " + /* bus.getMaxSpeed() + " MPH */"with " + bus.getNumberOfOccupants() + " occupants inside.");
+
+        Jet fastJet = new Jet(2000);
+        fastJet.setNumberOfOccupants(1);
+        fastJet.start();
+        System.out.println("Zooming in a fast jet at " + /* fastJet.getMaxSpeed() + " MPH */"with " + fastJet.getNumberOfOccupants() + " occupants inside.");
     }
 }
