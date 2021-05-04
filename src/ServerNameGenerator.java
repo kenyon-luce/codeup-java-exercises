10bdec57f980fedf66100ff1f48109ac19bb0ef7
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-04 11:21:13 -0500

    small grammar changes

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ServerNameGenerator {
     public static void main(String[] args){
         String[] adjectives = {"sad", "happy", "wise", "foolish", "fast", "slow", "red", "orange", "yellow", "green", "blue", "purple",};
-        String[] nouns = {"wolf", "moose", "rooster", "Dan", "Bert", "Joe", "car", "bus", "plane", "bike", "house", "lily"};
+        String[] nouns = {"wolf", "moose", "rooster", "dan", "bert", "joe", "car", "bus", "plane", "bike", "house", "lily"};
 
 //        System.out.println(randomElement(adjectives));
 //        System.out.println(randomElement(nouns));
