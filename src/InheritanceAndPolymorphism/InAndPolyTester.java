d2f541ebaad9860666f053073c2aed81450d7b99
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-04 14:31:45 -0500

    added spawn vehicle

--- src/InheritanceAndPolymorphism/InAndPolyTester.java
+++ src/InheritanceAndPolymorphism/InAndPolyTester.java
@@ -17,5 +17,26 @@ public class InAndPolyTester {
         fastJet.setNumberOfOccupants(1);
         fastJet.start();
         System.out.println("Zooming in a fast jet at " + /* fastJet.getMaxSpeed() + " MPH */"with " + fastJet.getNumberOfOccupants() + " occupants inside.");
+
+        System.out.println();
+        Vehicle fasterBus = spawnVehicle("bus", 120);
+        fasterBus.start();
+    }
+
+    public static Vehicle spawnVehicle(String vehicleType, int maxSpeed){
+        Vehicle vehicle = null;
+
+        switch (vehicleType) {
+            case "vehicle":
+                vehicle = new Vehicle(maxSpeed);
+                break;
+            case "bus":
+                vehicle = new Bus(maxSpeed);
+                break;
+            case "jet":
+                vehicle = new Jet(maxSpeed);
+            break;
+        }
+        return vehicle;
     }
 }
