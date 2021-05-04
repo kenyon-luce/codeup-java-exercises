d768752cdcc17fd86d8744e0dfe0f56e8873346f
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-04 15:08:50 -0500

    added rectangle

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,19 @@
+package shapes;
+
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea(){
+        return length * width;
+    }
+
+    public double getParameter(){
+        return (length * 2) + (width * 2);
+    }
+}
