ff99eef0847dec10acf5e91b772a9ab2b7da42ad
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-04 10:22:24 -0500

    updated exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
         //^this only prints strings, to print the numbers array we need to convert it to a string
@@ -10,15 +10,31 @@ public class ArraysExercises {
 
 //        Arrays[] container = new Arrays[3];
 //        String[] people = {"person1", "person2", "person3"};
+        people[0] = new Person("person1");
+        people[1] = new Person("person2");
+        people[2] = new Person("person3");
 
-//        for(Person person : people ){
-//            System.out.println(person);
+//        for(Person name : people ){
+//            System.out.println(name);
 //        }
 
-        System.out.print(Arrays.toString(people));
+//        for (int i = 0; i < people.length; i++) {
+//            System.out.println(people[i].getName());
+//        }
+        Person kenyon = new Person("Kenyon");
+        addPerson(people, kenyon);
+
+        for (int i = 0; i < people.length + 1; i++) {
+            System.out.println(addPerson(people, kenyon)[i].getName());
+        }
+
+//        System.out.print(Arrays.toString(people));
     }
-//    public Person[] addPerson(){
-//
-//    }
+
+    public static Person[] addPerson(Person[] people, Person newPerson) {
+        Person[] newPeople = Arrays.copyOf(people, people.length + 1);
+        newPeople[people.length - 1] = newPerson;
+        return newPeople;
+    }
 
 }
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -6,11 +6,12 @@ public class ServerNameGenerator {
     public static void main(String[] args){
         String[] adjectives = {"sad", "happy", "wise", "foolish", "fast", "slow", "red", "orange", "yellow", "green", "blue", "purple",};
         String[] nouns = {"wolf", "moose", "rooster", "Dan", "Bert", "Joe", "car", "bus", "plane", "bike", "house", "lily"};
-        Random randomAdj = new Random(adjectives.length);
-        Random randomNoun = new Random(nouns.length);
+//        Random randomAdj = new Random(adjectives.length);
+//        Random randomNoun = new Random(nouns.length);
+//        System.out.println(randomAdj);
+//        System.out.println(nouns[1]);
+//        public static int Math.random() * (adjectives.length + 1);
 
-        System.out.println(randomAdj);
-        System.out.println(randomNoun);
     }
 }
 
