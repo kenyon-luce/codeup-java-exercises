b27a025bcea9c54d2c30f153dffbdd5a6f84d839
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-04 15:19:52 -0500

    added square

---
+++ src/shapes/Square.java
@@ -0,0 +1,10 @@
+package shapes;
+
+public class Square extends Rectangle{ //extends rectangle??
+    private double side;
+
+    public Square(double length, double width, double side){
+        super(length, width);
+        this.side = side;
+    }
+}
