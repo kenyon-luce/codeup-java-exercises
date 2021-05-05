7ce9ac1f90a4dbc754912984d1abea514c679cb2
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-05 10:00:38 -0500

    added movies app

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,10 @@
+package movies;
+
+import util.Input;
+
+public class MoviesApplication {
+    public static void main(String[] args){
+        System.out.println(MoviesArray.findAll());
+        Input.getInt();
+    }
+}
