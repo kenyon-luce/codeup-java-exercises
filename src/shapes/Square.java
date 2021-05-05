23affd426c8a25421a974578436ead79008f0c8d
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-05 09:29:53 -0500

    changed private to protected

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,7 +1,7 @@
 package shapes;
 
 public class Square extends Rectangle{ //extends rectangle??
-    private double side;
+    protected double side;
 
     public Square(double side){
         super(side, side); //extends from rectangle so it needs the 2 parameters to overwrite them, since this is a square we will overwrite both with one variable (side)
