9a8e06a31e7f60361e8a59b59e3f1a07199355f9
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-06 13:40:27 -0500

    beginning interface exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,32 +1,32 @@
 package shapes;
 
 public class Rectangle {
-    protected double length;
-    protected double width;
-
-    public Rectangle(double length, double width){
-        this.length = length;
-        this.width = width;
-    }
-
-    public double getArea(){
-        double area = length * width;
-        System.out.println("Area: " + area);
-
-        return area;
-    }
-
-    public double getPerimeter(){
-        double perimeter = (length * 2) + (width * 2);
-        System.out.println("Perimeter: " + perimeter);
-
-        return perimeter;
-    }
-
-    public String getMeasurements(){
-        String measurements = "  (length:" + this.length + ", width:" + this.width + ")\n";
-        System.out.print(measurements);
-
-        return measurements;
-    }
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle(double length, double width){
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getArea(){
+//        double area = length * width;
+//        System.out.println("Area: " + area);
+//
+//        return area;
+//    }
+//
+//    public double getPerimeter(){
+//        double perimeter = (length * 2) + (width * 2);
+//        System.out.println("Perimeter: " + perimeter);
+//
+//        return perimeter;
+//    }
+//
+//    public String getMeasurements(){
+//        String measurements = "  (length:" + this.length + ", width:" + this.width + ")\n";
+//        System.out.print(measurements);
+//
+//        return measurements;
+//    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,33 +1,33 @@
 package shapes;
 
 public class Square extends Rectangle{ //extends rectangle??
-    protected double side;
+//    protected double side;
 
-    public Square(double side){
-        super(side, side); //extends from rectangle so it needs the 2 parameters to overwrite them, since this is a square we will overwrite both with one variable (side)
-        this.side = side;
-    }
-
-    public double getArea() {
-//        super.getArea();
-        double area = Math.pow(side, 2);
-        System.out.println("Area: " + area);
-
-        return area;
-    }
-
-    public double getPerimeter() {
-//        super.getPerimeter();
-        double perimeter = side * 4;
-        System.out.println("Perimeter: " + perimeter);
-
-        return perimeter;
-    }
-
-    public String getMeasurements(){
-        String measurements = "  (length:" + this.side + ", width:" + this.side + ")\n";
-        System.out.print(measurements);
-
-        return measurements;
-    }
+//    public Square(double side){
+//        super(side, side); //extends from rectangle so it needs the 2 parameters to overwrite them, since this is a square we will overwrite both with one variable (side)
+//        this.side = side;
+//    }
+//
+//    public double getArea() {
+////        super.getArea();
+//        double area = Math.pow(side, 2);
+//        System.out.println("Area: " + area);
+//
+//        return area;
+//    }
+//
+//    public double getPerimeter() {
+////        super.getPerimeter();
+//        double perimeter = side * 4;
+//        System.out.println("Perimeter: " + perimeter);
+//
+//        return perimeter;
+//    }
+//
+//    public String getMeasurements(){
+//        String measurements = "  (length:" + this.side + ", width:" + this.side + ")\n";
+//        System.out.print(measurements);
+//
+//        return measurements;
+//    }
 }
