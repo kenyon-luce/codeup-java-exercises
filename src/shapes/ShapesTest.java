abb3aec487c262456cbddb9accd340105e399c33
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-06 13:47:52 -0500

    exercise parts 1 - 3 complete

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,19 +4,19 @@ public class ShapesTest {
     public static void main(String[] args){
         System.out.print("[Rectangle]");
 
-        Rectangle box1 = new Rectangle(5, 4);
+//        Rectangle box1 = new Rectangle(5, 4);
 
-        box1.getMeasurements();
-        box1.getPerimeter();
-        box1.getArea();
+//        box1.getMeasurements();
+//        box1.getPerimeter();
+//        box1.getArea();
         System.out.println();
 
         System.out.print("[Square]");
 
-        Rectangle box2 = new Square(5); //just want to pass side, side overwrites Rectangles length and width parameters
+//        Rectangle box2 = new Square(5); //just want to pass side, side overwrites Rectangles length and width parameters
 
-        box2.getMeasurements();
-        box2.getPerimeter();
-        box2.getArea();
+//        box2.getMeasurements();
+//        box2.getPerimeter();
+//        box2.getArea();
     }
 }
