d8d36c6e5b2b36dd549b38e01b1fef1cc28745e5
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-08 11:38:47 -0500

    add items working

--- src/groceries/GroceriesApplication.java
+++ src/groceries/GroceriesApplication.java
@@ -4,21 +4,30 @@ import java.util.Scanner;
 
 public class GroceriesApplication {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Create a grocery list?");
-        String initialize = sc.next();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Create a grocery list?");
+//        String initialize = sc.next();
 
-        if (initialize.startsWith("y")) {
-            System.out.println("ok");
-            System.out.println(
-                    "1 - Beverages\n" +
-                    "2 - Bread/Bakery\n" +
-                    "3 - Canned/Jarred Goods\n" +
-                    "4 - Dairy\n" +
-                    "5 - Frozen Foods\n" +
-                    "6 - Meat\n" +
-                    "7 - Produce\n"
-            );
-        }
+//        if (initialize.startsWith("y")) {
+//            System.out.println("ok");
+//            System.out.println(
+//                    "1 - Beverages\n" +
+//                    "2 - Bread/Bakery\n" +
+//                    "3 - Canned/Jarred Goods\n" +
+//                    "4 - Dairy\n" +
+//                    "5 - Frozen Foods\n" +
+//                    "6 - Meat\n" +
+//                    "7 - Produce\n"
+//            );
+//        }
+
+        Item newItem = new Item("bread", "bread");
+        System.out.println(newItem.getItemName());
+        System.out.println(newItem.getCategory());
+//        System.out.println(newItem.defaultQuantity);
+        System.out.println(newItem.getQuantity());
+        System.out.println(newItem.setQuantity(2));
+        System.out.println(newItem.getQuantity());
+
     }
 }
--- src/groceries/Item.java
+++ src/groceries/Item.java
@@ -1,11 +1,31 @@
 package groceries;
 
-public class Groceries {
-    private String itemName;
-    private int itemQuantity;
+public class Item {
+    private String name;
     private String category;
+    private int quantity;
 
-    public Groceries(String itemName){
+    public Item(String itemName, String category){
+        this.name = itemName;
+        this.category = category;
+        this.quantity = 1;
+    }
 
+    public String getItemName(){
+//        System.out.println(this.itemName);
+        return this.name;
+    }
+
+    public String getCategory(){
+//        System.out.println(this.category);
+        return this.category;
+    }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public int setQuantity(int amount){
+        return this.quantity = amount;
     }
 }
