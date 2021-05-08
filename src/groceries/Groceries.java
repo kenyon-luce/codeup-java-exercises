ec9d064e9155e673d92cfc0f6c8e9df8e3989171
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-08 10:58:51 -0500

    starting groceries exercise

--- src/groceries/Groceries.java
+++ src/groceries/Groceries.java
@@ -1,4 +1,11 @@
 package groceries;
 
 public class Groceries {
+    private String itemName;
+    private int itemQuantity;
+    private String category;
+
+    public Groceries(String itemName){
+
+    }
 }
--- src/groceries/GroceriesApplication.java
+++ src/groceries/GroceriesApplication.java
@@ -3,13 +3,22 @@ package groceries;
 import java.util.Scanner;
 
 public class GroceriesApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Create a grocery list?");
         String initialize = sc.next();
 
-        if(initialize.startsWith("y")){
+        if (initialize.startsWith("y")) {
             System.out.println("ok");
+            System.out.println(
+                    "1 - Beverages\n" +
+                    "2 - Bread/Bakery\n" +
+                    "3 - Canned/Jarred Goods\n" +
+                    "4 - Dairy\n" +
+                    "5 - Frozen Foods\n" +
+                    "6 - Meat\n" +
+                    "7 - Produce\n"
+            );
         }
     }
 }
