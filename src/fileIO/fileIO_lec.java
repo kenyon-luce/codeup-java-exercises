a99001ab19b2db5ede278d367e1eb221b495061d
Kenyon Luce <kenyon.luce02@gmail.com>
2021-05-10 15:11:25 -0500

    added poem function

--- src/fileIO/fileIO_lec.java
+++ src/fileIO/fileIO_lec.java
@@ -3,6 +3,7 @@ package fileIO;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class fileIO_lec {
@@ -14,7 +15,9 @@ public class fileIO_lec {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        Path filePathPoem = Paths.get("./src/fileIO/poem.txt");
 
+        fileIO_lec.printPoem(filePathPoem); //prints poem and uses loop to number each line
     }
 }
