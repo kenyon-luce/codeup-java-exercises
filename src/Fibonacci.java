354d799d2839ff3e13d4d82453b539f168afc985
Kenyon Luce <kenyon.luce02@gmail.com>
2021-06-03 11:41:15 -0500

    updated long

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,9 +1,9 @@
 public class Fibonacci {
     public static void main(String[] args){
-        int a = 0;
-        int b = 1;
-        int c;
-        for (int i = 0; i <= 20; i++){
+        long a = 0;
+        long b = 1;
+        long c;
+        for (long i = 0; i <= 50; i++){
             c = a; //0 ; 1 ; 2 ; 3 ;
             a = a + b; //0 + 1 = 1 ; 1 + 1 = 2 ; 2 + 1 = 3 ; 3 + 2 = 5 ;
             b = c; // 1 --> 1 ; 1 --> 1 ; 1 --> 2 ; 2 --> 5
